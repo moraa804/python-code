be848ad0c60d9a2daa94356466b044c693939eb3
moraa804 <nancymoraa207@gmail.com>
2026-01-08 00:36:21 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,18 @@
+first =input("first: ")
+second =input("second: ")
+sum= int(first)+int(second)
+print("sum: " +str(sum))
+course= 'learnig python'
+print(course.upper())
+name = 'my name is nancy'
+print(name.upper())
+price=10
+print(price >5 and price<30)
+print(not price>10)
+temperature=35
+if temperature>30:
+  print("it's a hote day")
+  print("drik alot of wter")
+
+elif temperature<20:
+  print("it's a nice day")
